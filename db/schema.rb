d335c29d76ecbf5b57832e67d922c4e4ac3adab1
Brayan Lopez <brayan.lopez@koombea.com>
2020-07-21 10:19:03 -0500

    Added security model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_21_151331) do
+ActiveRecord::Schema.define(version: 2020_07_21_151831) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,6 +60,12 @@ ActiveRecord::Schema.define(version: 2020_07_21_151331) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["floor_id"], name: "index_offices_on_floor_id"
+  end
+
+  create_table "securities", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "tokens", force: :cascade do |t|
