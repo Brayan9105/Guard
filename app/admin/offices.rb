0c1ad464d3911269d2c50844bfa77c0cd06a1414
Brayan Lopez <brayan.lopez@koombea.com>
2020-07-25 08:36:46 -0500

    Added active admin resources

---
+++ app/admin/offices.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Office do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :name, :floor_id
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:name, :floor_id]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
---
+++ app/admin/visitors.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Visitor do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :dni, :first_name, :last_name, :health_id, :security_id, :status, :photo
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:dni, :first_name, :last_name, :health_id, :security_id, :status, :photo]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
